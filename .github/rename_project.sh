d874364c7246bf3fc484dd23f2cfd41fe2d89309
asrulsibaoel <asrulsibaoel@users.noreply.github.com>
2022-07-21 05:22:39 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="asrulsibaoel"
+original_name="python_packages_template"
+original_urlname="python-packages-template"
+original_description="Awesome python_packages_template created by asrulsibaoel"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv python_packages_template $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
